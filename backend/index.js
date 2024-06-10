4735c70cec44628821e411f8b2f6041a353fa2ad
sandithi1117 <110651880+sandithiV@users.noreply.github.com>
2024-06-10 18:33:16 +0530

    created database connection
    created database connection

--- backend/index.js
+++ backend/index.js
@@ -1,8 +1,16 @@
 const express = require('express');
 const dotenv = require('dotenv').config()
-const cors = require('cors')
+const cors = require('cors');
+const {mongoose} = require('mongoose')
+
+//database connection
+mongoose.connect(process.env.MONGO_URL)
+.then(() => console.log('Database Connected'))
+.catch(() => console.log('Database not connected', err))
 
 const app = express();
+
+app.use('/', require('./routes/authRoutes'))
 
 const port = 8000;
 app.listen(port, () => console.log(`Server is running on port ${port}`))
