6fa99aa9754b860d99cc1818dccfe69d95a321cd
sandithi1117 <110651880+sandithiV@users.noreply.github.com>
2024-06-10 22:08:34 +0530

    Created JWT
    Json Web Token added

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,5 +1,6 @@
 const User = require('../models/user');
 const { hashPassword, comparePassword} = require('../helpers/auth');
+const jwt = require('jsonwebtoken');
 
 const test = (req, res) => {
     res.json('test is working')
@@ -70,7 +71,10 @@ const loginUser = async (req, res) => {
      // Check if password match
      const match = await comparePassword(password, user.password)
      if(match) {
-        res.json('passwords match')
+        jwt.sign({email: user.email, id: user._id, name: user.name}, process.env.JWT_SECRET, {}, (err,token) => {
+            if(err) throw err;
+            res.cookie('token', token).json(user)
+        })
      }
      if(!match) {
         res.json({
--- backend/index.js
+++ backend/index.js
@@ -3,6 +3,7 @@ const dotenv = require('dotenv').config()
 const cors = require('cors');
 const {mongoose} = require('mongoose')
 const app = express();
+const cookieParser = require ('cookie-parser')
 
 //database connection
 mongoose.connect(process.env.MONGO_URL)
@@ -10,9 +11,9 @@ mongoose.connect(process.env.MONGO_URL)
 .catch(() => console.log('Database not connected', err))
 
 //middleware
-app.use(express.json())
-
-
+app.use(express.json());
+app.use(cookieParser());
+app.use(express.urlencoded({extended: false}))
 
 app.use('/', require('./routes/authRoutes'))
 
